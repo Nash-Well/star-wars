3d889fd17ae57c8e9c56701423e70d46a13338ea
nash <nnash5826@gmail.com>
2024-07-19 17:31:41 +0300

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-star_wars_favorites-star_wars_favoritesTests/Pods-star_wars_favorites-star_wars_favoritesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_star_wars_favorites_star_wars_favoritesTests : NSObject
+@end
+@implementation PodsDummy_Pods_star_wars_favorites_star_wars_favoritesTests
+@end
